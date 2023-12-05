f4f3ba63afa484b0d04830979eb127f530dd57cd
bhavya416 <bhavyapadaliya179@gmail.com>
2023-12-05 14:47:06 -0800

    USER INPUT ADDED FOR NEW PREDICTION

--- Main.py
+++ Main.py
@@ -24,11 +24,15 @@ print("Mean Absolute Error:", mae)
 print("Mean Squared Error:", mse)
 print("R-squared:", r2)
 
+experience_year = float(input("Enter Year Of Experience You Have : "))
+education_level_code = int(input("1] Bachelors Degree\n2] Masters Degree\n3] Phd\nEnter From Above Choice (number) _ : "))
+
+profession_code = int(input("1] Software Developer\n2] Data Analyst\n3] Registered Nurse\n4] Teacher\n5] Accountant\n6] Marketing Manager\n7] Mechanical Engineer\n8] Medical Doctor\n9] Graphic Designer\n10] Electrician\nEnter From Above Choice (number) _ : "))
 
 new_data_point = pd.DataFrame({
-    "Experience_Years": [5],
-    "Profession_Code": [2],
-    "Education_Level_Code": [3]
+    "Experience_Years": [experience_year],
+    "Profession_Code": [profession_code],
+    "Education_Level_Code": [education_level_code]
 })
 
 predicted_salary = model.predict(new_data_point)
